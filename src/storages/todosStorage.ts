8d6499544e515c2178ccc44393df57fd84a0bc4a
jdwns96 <djjng3@gmail.com>
2022-05-22 18:44:34 +0900

    [FIN]

--- src/storages/todosStorage.ts
+++ src/storages/todosStorage.ts
@@ -1,4 +1,11 @@
 import AsyncStorage from "@react-native-community/async-storage";
+/**
+ * ios 에서는 용량이 따로 정해져 있지 않다.
+ *
+ * android 에서는 default 6MB
+ * 최대 용량을 늘리고 싶으면 android/gradle.properties 에서
+ * AsyncStorage_db_size_in_MB=10
+ */
 
 const key = "todos";
 
